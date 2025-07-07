c9aad066233c45d242b9fc443e15909fd66567ec
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-07-07 14:38:34 +0500

    working on progress

--- lib/LoginScreens/verify_email/Verify_Email.dart
+++ lib/LoginScreens/verify_email/Verify_Email.dart
@@ -1,2 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+class VerifyEmail extends StatelessWidget {
+  const VerifyEmail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Verify your email 2 / 3', style: GoogleFonts.jost(
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            color: Color(0xff000000))
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Center(
+            child:Text('         We just sent 5-digit codee to\nalexalexander@gmail.com,enter it bellow:',style: GoogleFonts.roboto(
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+              color: Color(0xff27252E)
+            ),),
+          )
+        ],
+      ),
+    );
+  }
+}
 
