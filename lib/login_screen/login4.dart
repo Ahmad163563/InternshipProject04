be79dd2e3851bad1c1df031a4988b680b50969b4
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-07-08 12:13:56 +0500

    logins screen
    creating login screens

---
+++ lib/login_screen/login4.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project/components/custom_button/custom_button.dart';
+
+class Login4 extends StatefulWidget {
+  const Login4({super.key});
+
+  @override
+  State<Login4> createState() => _Login4State();
+}
+
+class _Login4State extends State<Login4> {
+  bool _obscuretext=true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Log into account', style: GoogleFonts.jost(
+            fontSize: 20,
+            fontWeight: FontWeight.w400,
+            color: Color(0xff000000))
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 50,),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Text('Email',style: GoogleFonts.jost(
+                fontWeight: FontWeight.w400,
+                color: Color(0xff121A2C),
+                fontSize: 16
+            ),),
+          ),
+          SizedBox(height: 5,),
+          Center(
+            child: Container(
+              padding: EdgeInsets.only(left: 10),
+              //alignment: Alignment.center,
+              height: 48,
+              width: 341,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'alexalexander@gmail.com'
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          Padding(
+            padding: EdgeInsets.only(left: 20.0),
+            child: Text('Password',style: GoogleFonts.jost(
+                fontWeight: FontWeight.w400,
+                fontSize: 16,
+                color: Color(0xff121A2C)
+            ),),
+          ),
+          SizedBox(height: 5,),
+          Center(
+            child: Container(
+              padding: EdgeInsets.only(left: 10),
+              height: 48,
+              width: 341,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all()
+              ),
+              child: TextField(
+                obscureText: _obscuretext,
+                decoration: InputDecoration(
+                    hintText: '********',
+                    border: InputBorder.none,
+                    suffixIcon: IconButton(onPressed: (){
+                      setState(() {
+                        _obscuretext=!_obscuretext;
+                      });
+                    }, icon: Icon(_obscuretext?Icons.visibility_off:Icons.visibility))
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          Center(child: CustomButton(text: 'Log in', onTap: (){})),
+          SizedBox(height: 20,),
+          Center(
+            child:
+            Text('Forgot password?',style: GoogleFonts.jost(
+                fontSize: 14,
+                fontWeight: FontWeight.w600
+            ),),
+          ),
+          Spacer(),
+          Center(
+            child: RichText(
+              text: TextSpan(
+                text: 'By using Solvbox, you agree to the ', // First part
+                style:GoogleFonts.roboto(fontSize: 14,color: Color(0xff605D67)),
+                children: <TextSpan>[
+                  TextSpan(
+                      text: '\n      Terms', // Second part
+                      style:GoogleFonts.roboto(fontSize: 14,color: Color(0xff605D67),fontWeight: FontWeight.w700)
+                  ),
+                  TextSpan(
+                      text: '  and', // Third part
+                      style: GoogleFonts.roboto(fontSize: 14,color: Color(0xff605D67))
+                  ),
+                  TextSpan(
+                      text: '  Privacy Policy',style: GoogleFonts.roboto(
+                      fontSize: 14,
+                      color: Color(0xff605D67),
+                      fontWeight: FontWeight.w700
+                  )
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:project/SignUp_Screens/account_creation/account_creation.dart';
 import 'package:project/SignUp_Screens/apply_Screen/Problem_solver/problem_solver.dart';
 import 'package:project/SignUp_Screens/apply_Screen/apply%20screen.dart';
+import 'package:project/SignUp_Screens/apply_Screen/problem_seeker/problem_seeker.dart';
 import 'package:project/SignUp_Screens/create_pass/create_password.dart';
 
 import 'package:project/SignUp_Screens/verify_email/Verify_Email.dart';
+import 'package:project/login_screen/login4.dart';
+import 'package:project/login_screen/login_into_account3.dart';
+import 'package:project/login_screen/loginaccount1.dart';
+import 'package:project/login_screen/loginscreen2.dart';
 import 'package:project/starting_/SplashScreen/splashscreen.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -24,7 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProblemSolver()//ApplyScreen()//AccountCreation()//CreatePassword()//VerifyEmail()//AddingEmail(currentStep: 1)//CreateAccount()//Splashscreen()
+      home: Login4()//LoginIntoAccount()//Loginaccount()//LoginApplyScreen()//ProblemSeeker()//ProblemSolver()//ApplyScreen()//AccountCreation()//CreatePassword()//VerifyEmail()//AddingEmail(currentStep: 1)//CreateAccount()//Splashscreen()
     );
   }
 }
